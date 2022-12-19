78d179b348e90b7759d5d66db0901e3976b75272
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2022-12-20 01:30:51 +0900

    logコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :logs
+  get 'logs/index'
+  get 'logs/new'
+  get 'logs/show'
   get 'top/home'
   root 'top#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
