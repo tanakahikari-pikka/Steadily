a09605c77a1bc3dfb07de3984c8e3ff7b9e0bd6f
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2022-12-20 01:14:46 +0900

    logコントローラー作成

---
+++ test/controllers/logs_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class LogsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get logs_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get logs_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get logs_show_url
+    assert_response :success
+  end
+end
