57f44dc97f954303a6a6b0e98889251cec0ac971
Hikari-Tanaka-pikka <h.tanaka.22.7.575@4nonome.com>
2023-01-04 18:27:18 +0900

    バリデーション・エラーメッセージ実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   has_many :logs, dependent: :destroy
   has_many :users, :through => :logs, dependent: :destroy
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true, length: {maximum: 255}
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,9 @@
 class Log < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true, length: {maximum: 255}
+  validates :category, presence: true, length: {maximum: 255}
+  validates :achieve, presence: true, length: {maximum: 2056}
+  validates :study_record, presence: true
 end
